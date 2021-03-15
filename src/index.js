86ab71cb1c92bf66f9cabb3c7a9f715cfc21ed19
Katya Litvinchuk <77808444+kathleenlii@users.noreply.github.com>
2021-03-15 23:04:58 +0300

    fiat: add readable-number implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    var num = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    var tens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 20) {
+        return num[number];
+    }
+
+    var digit = number % 10;
+
+    if (number < 100) {
+        const numberString = digit ? ` ${num[digit]}` : "";
+
+        return `${tens[Math.floor(number / 10) - 2]}${numberString}`;
+    }
+
+    const tensString = number % 100 == 0 ? "" : ` ${toReadable(number % 100)}`;
+
+    return `${num[Math.floor(number / 100)]} hundred${tensString}`;
 }
